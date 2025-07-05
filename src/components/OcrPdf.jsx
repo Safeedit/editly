93d0db677bc90e1d9d52021cde5c65e77e99a17f
Safeedit <safeeditpdf@gmail.com>
2025-07-05 10:33:17 +0530

    Update OcrPdf.jsx

--- src/components/OcrPdf.jsx
+++ src/components/OcrPdf.jsx
@@ -15,7 +15,7 @@ function OcrPdf() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5001/convert", {
+      const res = await fetch("https://editlybackend.onrender.com/convert", {
         method: "POST",
         body: formData,
       });
